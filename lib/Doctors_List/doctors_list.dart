62abd606f22a3442ca1ad1aa355430c93ff30144
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-07 16:39:11 +0600

    okay

--- lib/Doctors_List/doctors_list.dart
+++ lib/Doctors_List/doctors_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:health_care/Doctors_List/first_card.dart';
+import 'package:health_care/Doctors_List/second_card.dart';
 
 class DoctorsHome extends StatelessWidget {
   const DoctorsHome({Key? key}) : super(key: key);
@@ -39,61 +41,8 @@ class DoctorsHome extends StatelessWidget {
           Expanded(
               child: ListView.builder(
                   itemCount: 7,
-                  itemBuilder: ((context, index) => Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                height: 200,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                    color: Colors.yellowAccent,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Image.asset(
-                                        'images/dt.png',
-                                        height: 70,
-                                        width: 50,
-                                      ),
-                                      const SizedBox(
-                                        width: 15,
-                                      ),
-                                      Column(
-                                        children: const [
-                                          Text(
-                                            'General Physician',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          SizedBox(
-                                            height: 25,
-                                          ),
-                                          Text(
-                                              'Lorem ipsum is placeholder text commonly \n used in the graphic')
-                                        ],
-                                      ),
-                                      const SizedBox(
-                                        width: 15,
-                                      ),
-                                      const Icon(Icons.arrow_forward_ios)
-                                    ],
-                                  ),
-                                )),
-                          ),
-                          Container(
-                            height: 300,
-                            width: double.infinity,
-                            color: Colors.red,
-                          ),
-                        ],
-                      ))))
+                  itemBuilder: ((context, index) =>
+                      Column(children: const [CardWithColum(), SecondCard()]))))
         ],
       ),
     );
