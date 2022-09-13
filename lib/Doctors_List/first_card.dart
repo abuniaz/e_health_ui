fe0cde25a064e6bbfc1716e8681a95998387db5d
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-13 19:22:18 +0600

    Update first_card.dart

--- lib/Doctors_List/first_card.dart
+++ lib/Doctors_List/first_card.dart
@@ -14,49 +14,50 @@ class CardWithColum extends StatelessWidget {
       child: GestureDetector(
         onTap: press,
         child: Container(
-            height: 200,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 39, 199, 191),
-                borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'images/dt.png',
-                    height: 70,
-                    width: 50,
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Column(
-                    children: const [
-                      Text(
-                        'General Physician',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                          'Lorem ipsum is placeholder text commonly \n used in the graphic',
-                          style: TextStyle(color: Colors.black))
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Icon(Icons.arrow_forward_ios)
-                ],
-              ),
-            )),
+          height: 200,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 39, 199, 191),
+              borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/dt.png',
+                  height: 70,
+                  width: 50,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  children: const [
+                    Text(
+                      'General Physician',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                        'Lorem ipsum is placeholder text commonly \n used in the graphic',
+                        style: TextStyle(color: Colors.black))
+                  ],
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                const Icon(Icons.arrow_forward_ios)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
