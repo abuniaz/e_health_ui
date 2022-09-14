791627ee529483fa489af17100eba4fb448ad0cf
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-14 18:45:26 +0600

    Update doctors_details.dart

--- lib/Doctors_List/doctors_details.dart
+++ lib/Doctors_List/doctors_details.dart
@@ -5,6 +5,38 @@ class DoctorDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Container(
+            height: 100,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+                      color: const Color.fromARGB(172, 255, 255, 255),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: "Search by doctor's name"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
