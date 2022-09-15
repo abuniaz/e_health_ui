edfd633f96e867cf33e4a52dcff3ea2569f6bfda
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-15 19:25:39 +0600

    Update doctors_details.dart

--- lib/Doctors_List/doctors_details.dart
+++ lib/Doctors_List/doctors_details.dart
@@ -5,58 +5,62 @@ class DoctorDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            height: 100,
-            width: double.infinity,
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Icon(
-                      Icons.arrow_back,
-                      color: Colors.black,
-                    ),
-                    Container(
-                      height: 45,
-                      width: 300,
-                      decoration: BoxDecoration(
-                          color: Colors.teal,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const TextField(
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        height: 45,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.teal,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: TextField(
                           decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: "Search doctor's name")),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Text('100 doctors found for medicine',
-                        style: TextStyle(fontSize: 16, color: Colors.black)),
-                    ABoxDecoration(
-                      icon: Icon(Icons.filter_list),
-                      title: 'Filter',
-                    ),
-                    ABoxDecoration(
-                      icon: Icon(Icons.arrow_downward),
-                      title: 'Sort by',
-                    ),
-                  ],
-                ),
-              ],
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              hintText: "Search doctor's name"),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text('100 doctors found for medicine',
+                          style: TextStyle(fontSize: 16, color: Colors.black)),
+                      ABoxDecoration(
+                        icon: Icon(Icons.filter_list),
+                        title: 'Filter',
+                      ),
+                      ABoxDecoration(
+                        icon: Icon(Icons.arrow_downward),
+                        title: 'Sort by',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
